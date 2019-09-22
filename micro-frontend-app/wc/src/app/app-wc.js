c2b7ed9a3e39b64b2d9dd88976af912f9e8f3686
“teerasak” <teerasak@thoughtworks.com>
2019-09-23 00:14:27 +0700

    wc: add q attribute and apply into app-shell

--- micro-frontend-app/wc/src/app/app-wc.js
+++ micro-frontend-app/wc/src/app/app-wc.js
@@ -4,17 +4,40 @@ class A extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const wrapper = this.render();
     shadow.appendChild(wrapper);
+
+    this.searchElem = this.shadowRoot.getElementById('searchElem');
+  }
+
+  static get observedAttributes() {
+    return ['q'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue == newValue) return;
+
+    switch (name) {
+      case 'q':
+        this[name] = newValue;
+        this.searchElem.value = newValue;
+        break;
+    }
+  }
+
+  get q() {
+    return this.getAttribute('q') || '';
+  }
+
+  set q(newValue) {
+    this.setAttribute('q', newValue);
   }
 
   renderContainer() {
     const msg = document.createElement("div");
     msg.innerHTML = `
      <form method="get" action="http://google.com/search">
-
-          <input type="text" name="q" required autofocus>
+          <input type="text" id="searchElem" name="q" value="` + this.q + `" required autofocus>
           <input type="submit" value="Google search">
-
-      </form>
+     </form>
     `
     return msg;
   }
