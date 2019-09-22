10b7fbe6c3785c3f283df40457784fdad9e700e7
“teerasak” <teerasak@thoughtworks.com>
2019-09-22 21:28:09 +0700

    wc: change to google search

--- micro-frontend-app/wc/src/app/app-wc.js
+++ micro-frontend-app/wc/src/app/app-wc.js
@@ -1,23 +1,30 @@
 class A extends HTMLElement {
-	constructor() {
-		super();
-		const shadow = this.attachShadow({ mode: "open" });
-		const wrapper = this.render();
-		shadow.appendChild(wrapper);
-	}
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const wrapper = this.render();
+    shadow.appendChild(wrapper);
+  }
 
-	renderContainer() {
-		const msg = document.createElement("span");
-		msg.innerHTML = `hello from micro-frontend A!`;
-		return msg;
-	}
+  renderContainer() {
+    const msg = document.createElement("div");
+    msg.innerHTML = `
+     <form method="get" action="http://google.com/search">
 
-	render() {
-		const wrapper = document.createElement("div");
-		wrapper.className = "container";
-		wrapper.append(this.renderContainer());
-		return wrapper;
-	}
+          <input type="text" name="q" required autofocus>
+          <input type="submit" value="Google search">
+
+      </form>
+    `
+    return msg;
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.className = "container";
+    wrapper.append(this.renderContainer());
+    return wrapper;
+  }
 }
 
 customElements.define("app-wc", A);
