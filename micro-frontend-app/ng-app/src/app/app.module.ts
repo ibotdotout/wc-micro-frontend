d95fa495d3b34f091e90159f44871301bd6cc37a
“teerasak” <teerasak@thoughtworks.com>
2019-09-22 18:21:43 +0700

    ng: convert angular into web component by angular element

--- micro-frontend-app/ng-app/src/app/app.module.ts
+++ micro-frontend-app/ng-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,10 +9,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(private injector: Injector) {
+        const el = createCustomElement(AppComponent, { injector });
+        customElements.define('app-ng', el);
+    }
+
+	ngDoBootstrap(){}
+}
