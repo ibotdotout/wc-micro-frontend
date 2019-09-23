10c33a85f31e6bcd91f2151ba1266c15326fc8d7
“teerasak” <teerasak@thoughtworks.com>
2019-09-23 08:15:36 +0700

    react: add web component inside react

--- micro-frontend-app/react-app/src/App.js
+++ micro-frontend-app/react-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import banner from './banner.png';
+import wc from './wc';
 
 function App() {
   return (
     <div className="App">
       <img src={banner}></img>
+    { /* web component inside react */ }
+      <mf-link n="3"></mf-link>
     </div>
   );
 }
---
+++ micro-frontend-app/react-app/src/wc.js
@@ -0,0 +1,62 @@
+class MicroFrontendLink extends HTMLElement {
+
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    this.items = {
+      'Micro-frontends.org': 'https://micro-frontends.org/',
+      'Martin Fowler: micro-frontends': 'https://martinfowler.com/articles/micro-frontends.html',
+      'Web Components': 'https://www.webcomponents.org/',
+      'Code on Github': 'https://github.com/ibotdotout/wc-micro-frontend',
+      'Monolith -> Micro Frontends': 'https://dev.to/aregee/breaking-down-the-last-monolith-micro-frontends-hd4',
+    }
+
+    const wrapper = this.render();
+    shadow.appendChild(wrapper);
+
+    this.container = this.shadowRoot.querySelector(".container");
+  }
+
+  static get observedAttributes() {
+    return ['n'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue == newValue) return;
+
+    switch (name) {
+      case 'n':
+        this[name] = newValue;
+        this.container.replaceChild(this.renderContainer(), this.container.childNodes[0]);
+        break;
+    }
+  }
+
+  get n() {
+    const MAX_SIZE = Object.keys(this.items).length;
+    const n = Math.max(0, Math.min(this.getAttribute('n') || MAX_SIZE, MAX_SIZE));
+    return n;
+  }
+
+  set n(newValue) {
+    this.setAttribute('n', parseInt(newValue));
+  }
+
+  renderContainer() {
+    const msg = document.createElement("div");
+    msg.innerHTML = Object.keys(this.items)
+      .filter((_, i) => i < this.n)
+      .map(key =>  '<a href="' + this.items[key] + '">' + key + '</a>')
+      .join("  ///  ");
+    return msg;
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.className = "container";
+    wrapper.append(this.renderContainer());
+    return wrapper;
+  }
+}
+
+customElements.define("mf-link", MicroFrontendLink);
